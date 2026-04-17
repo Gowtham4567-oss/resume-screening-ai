858ec873c783127fcb8b1dcdb23703a721662a1f
Gowtham4567-oss <gowtha4567@gmail.com>
2026-04-17 17:32:03 +0530

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-async function uploadResume(){
-    document.getElementById("result").innerHTML="Resume analyzed successfully";
-}
-
-async function sendMessage(){
-    let input=document.getElementById("chatInput").value;
-    document.getElementById("chatBox").innerHTML += "<p><b>You:</b> "+input+"</p>";
-    document.getElementById("chatInput").value="";
-}
